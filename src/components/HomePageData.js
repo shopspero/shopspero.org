fbe23034b9c4c1b5b32abcda2800436bc07ae4cb
Jeffrey Liang <jeffrey.liang123@gmail.com>
2023-04-07 18:34:37 -0700

    Try to fix homepage links

--- src/components/HomePageData.js
+++ src/components/HomePageData.js
@@ -2,22 +2,22 @@ export const SliderData = [
   {
     image: require("../images/sliderimage1.jpg"),
     description: "NEW ARRIVALS",
-    link: process.env.PUBLIC_URL + "/shop",
+    link: "/shop",
   },
   {
     image: require("../images/sliderimage2.jpg"),
     description: "OUR MISSION",
-    link: process.env.PUBLIC_URL + "/about",
+    link: "/about",
   },
   {
     image: require("../images/sliderimage3.jpg"),
     description: "SHOP NOW",
-    link: process.env.PUBLIC_URL + "/shop",
+    link: "/shop",
   },
   {
     image: require("../images/sliderimage4.jpg"),
     description: "MEET THE TEAM",
-    link: process.env.PUBLIC_URL + "/team",
+    link: "/team",
   },
   {
     image: require("../images/sliderimage5.jpg"),
